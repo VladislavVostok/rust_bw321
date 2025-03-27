8413c6c27a75ecc9a6efad8e2395a861ef218b0e
Vladislav East <vladislav.sage@yandex.ru>
2025-03-27 18:41:32 +0300

    finsh

--- Blockchain/src/main.rs
+++ Blockchain/src/main.rs
@@ -18,15 +18,14 @@ struct Block{
     previous_hash: String,
     hash: String,
 }
-
 impl Block{
-    fn new(index: u64, transactions: Vec<Transaction>, previous_hash: String ) -> Block{
+    fn new(index: u64, transactions: Vec<Transaction>, previous_hash: String ) -> Block {
         let timestamp = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .unwrap()
             .as_secs();
 
-        let hash = Self::calculate_hash(index, timestamp, &transactions, &previous_hash)
+        let hash = Self::calculate_hash(index, timestamp, &transactions, &previous_hash);
 
         Block{
             index,
@@ -40,7 +39,7 @@ impl Block{
     fn calculate_hash(index: u64, timestamp: u64, transactions: &[Transaction], previous_hash: &str) -> String{
         let input = format!("{}{}{:?}{}", index, timestamp, transactions, previous_hash);
         let mut hasher = Sha256::new();
-        hasher.update(input);
+        Update::update(&mut hasher, input.as_bytes());
         let result = hasher.finalize();
 
         format!("{:x}", result)
@@ -62,7 +61,7 @@ impl Blockchain{
     }
 
     fn add_transaction(&mut self, transaction: Transaction){
-        self.pending_transactions.push((transaction));
+        self.pending_transactions.push(transaction);
     }
 
     fn mine_block(&mut self){
@@ -71,10 +70,95 @@ impl Blockchain{
         self.chain.push(new_block);
         self.pending_transactions.clear();
     }
+
+    fn is_chain_valid(&self) -> bool{
+        for i in 1..self.chain.len(){
+            let current_block = &self.chain[i];
+            let previous_block = &self.chain[i-1];
+
+            if current_block.hash != Block::calculate_hash(
+                current_block.index,
+                current_block.timestamp,
+                &current_block.transactions,
+                &current_block.previous_hash,
+            ){
+                println!("Подстава! Блок не валидный {}", current_block.index);
+                return false;
+            }
+
+            if current_block.previous_hash != previous_block.hash{
+                println!("Неверный хэш предыдущего блока{}", current_block.index);
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn print_block(&self){
+        for block in &self.chain{
+            println!("{:?}", block)
+        }
+    }
 }
 
 
 
 fn main() {
-    println!("Hello, world!");
+
+    let mut blockchain = Blockchain::new();
+
+    blockchain.add_transaction(Transaction
+        {
+            sender: "Александр".to_string(),
+            receiver: "Фируз".to_string(),
+            amount: 200.0,
+        }
+    );
+
+    blockchain.add_transaction(Transaction
+        {
+            sender: "Фируз".to_string(),
+            receiver: "Магазин спорт товаров \"Василиса\"".to_string(),
+            amount: 100.0,
+        }
+    );
+
+    blockchain.mine_block();
+
+    blockchain.add_transaction(Transaction
+    {
+        sender: "Никита".to_string(),
+        receiver: "Александр".to_string(),
+        amount: 1000.0,
+    }
+    );
+
+    blockchain.add_transaction(Transaction
+    {
+        sender: "Александр".to_string(),
+        receiver: "Данила".to_string(),
+        amount: 777.0,
+    }
+    );
+
+    blockchain.mine_block();
+
+    blockchain.print_block();
+
+    println!("Валидный ли Blockchain? {}", blockchain.is_chain_valid());
+
+    // blockchain.chain.push(Block{
+    //     index: 23515983,
+    //     timestamp: 238547983,
+    //     transactions: vec![],
+    //     hash: "ajikhdgoihqpw8e4uoihgtq47u9iyhtui4yh".to_string(),
+    //     previous_hash: "askdjfgpauoiwy38thsdoigju0pw498".to_string(),
+    // });
+    //
+    // blockchain.mine_block();
+    //
+    // blockchain.print_block();
+    //
+    // println!("Валидный ли Blockchain? {}", blockchain.is_chain_valid());
 }
