8110e5daf0f5f6cd6de1a8c7c7213822c67f680b
Vladislav East <vladislav.sage@yandex.ru>
2025-04-10 21:23:45 +0300

    markel root

--- Blockchain/src/main.rs
+++ Blockchain/src/main.rs
@@ -1,5 +1,8 @@
+use std::cmp::min;
+use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
 use serde::{Serialize, Deserialize};
+use serde_json::Value::String;
 use sha2::{Sha256, Digest};
 use sha2::digest::Update;
 
@@ -8,24 +11,67 @@ struct Transaction{
     sender: String,
     receiver: String,
     amount: f64,
+    timestamp: u64,
+    hash: String,
 }
+
+impl Transaction{
+    fn new(sender: String, receiver: String, amount: f64 ) -> Transaction {
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        let hash = Self::calculate_hash_tx(&sender, &receiver, amount, timestamp);
+
+        Transaction{
+            sender,
+            receiver,
+            amount,
+            timestamp,
+            hash,
+        }
+    }
+
+    fn calculate_hash_tx(sender: &str, receiver: &str, amount: f64, timestamp: u64) -> String {
+
+        let input = format!("{}{}{}{}", receiver, timestamp, amount, timestamp);
+        let mut hasher = Sha256::new();
+        Update::update(&mut hasher, input.as_bytes());
+        let result = hasher.finalize();
+
+        format!("{:x}", result)
+    }
+
+    fn print_transaction(&self){
+        print!("{} -> {} ({}) {} {}\n", self.sender, self.receiver, self.amount, self.hash, self.timestamp)
+    }
+}
+
+
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Block{
-    index: u64,
-    timestamp: u64,
-    transactions: Vec<Transaction>,
     previous_hash: String,
+    transactions: Vec<Transaction>,
     hash: String,
+    timestamp: u64,
+    nonce: u32,
+    merkele_root: String,
+    index: u64
 }
-impl Block{
+impl Block {
     fn new(index: u64, transactions: Vec<Transaction>, previous_hash: String ) -> Block {
         let timestamp = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .unwrap()
             .as_secs();
 
+        let merkele_root = Self::calculate_merkele_root(&transactions);
+
         let hash = Self::calculate_hash(index, timestamp, &transactions, &previous_hash);
+
+        let nonce = 0;
 
         Block{
             index,
@@ -33,6 +79,8 @@ impl Block{
             transactions,
             previous_hash,
             hash,
+            merkele_root,
+            nonce
         }
     }
 
@@ -44,21 +92,93 @@ impl Block{
 
         format!("{:x}", result)
     }
+
+    fn calculate_merkele_root(transactions: &Vec<Transaction>) -> String{
+
+        let mut hasher = Sha256::new();
+
+        if(transactions.is_empty()){
+            Update::update(&mut hasher, "".as_bytes());
+            return format!("{:x}", $hasher.finalize());
+        }
+
+        let mut tree : Vec<String> = Vec::new();
+
+        for tx in transactions {
+            Update::update(&mut hasher, tx.hash.as_bytes());
+            tree.push(format!("{:x}", $hasher.finalize()));
+        }
+
+        let mut  level_offset : u32 = 0;
+        let mut level_size = transactions.len();
+        while(level_size > 1){
+            let mut left:u32 = 0;
+
+            while(left < level_size){
+                let mut right = min(left + 1, level_size - 1);
+                Update::update(&mut hasher, format!("{}{}",tree[level_offset + left].hash, tree[level_offset + right].hash));
+                tree.push(format!("{:x}", $hasher.finalize()));
+
+                left += 2;
+            }
+            level_offset += level_size;
+
+
+            level_size = (level_size + 1) / 2;
+        }
+
+        let merkele_root_hash: String = match tree.is_empty() {
+
+            true => tree.last(),
+            false => String("")
+        }.unwrap();
+
+        merkele_root_hash
+    }
+
+    fn mine_block(&mut self, difficulty:u32){
+        let target = "0".repeat(4);
+
+
+        while(self.hash.starts_with(target.to_string())) {
+            self.nonce += 1;
+        }
+    }
 }
+
 
 struct Blockchain{
     chain: Vec<Block>,
+    difficulty: u32,
+    balances: HashMap<String, f64>,
     pending_transactions: Vec<Transaction>
 }
 
 impl Blockchain{
     fn new() -> Blockchain{
         let genesis_block = Block::new(0, vec![], "0".to_string());
+        let difficulty = 4;
+
         Blockchain{
             chain: vec![genesis_block],
             pending_transactions: vec![],
+            balances: HashMap::new(),
+            difficulty,
+
         }
     }
+
+    fn create_block(&self){
+
+        let previous_hash: String = match self.chain.is_empty() {
+
+            true => self.chain.last().unwrap().hash,
+            false => "0".to_string()
+        }.unwrap();
+
+
+    }
+
 
     fn add_transaction(&mut self, transaction: Transaction){
         self.pending_transactions.push(transaction);
@@ -70,6 +190,8 @@ impl Blockchain{
         self.chain.push(new_block);
         self.pending_transactions.clear();
     }
+
+
 
     fn is_chain_valid(&self) -> bool{
         for i in 1..self.chain.len(){
